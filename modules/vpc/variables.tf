14f5c1e6d6e267d878a7515a5613a1d3fe8f98ce
rokaroma <romanyjcb@gmail.com>
2026-01-28 15:09:18 +0200

    Add VPC variables for CIDR and subnets

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,1 +1,25 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "Availability Zones"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Environment name (dev/prod)"
+  type        = string
+}
 
